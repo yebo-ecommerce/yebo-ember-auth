19305baa7110706fa568b8da0c1b10875410bae6
yurifrl <yurifrl@hotmail.com>
2016-02-12 15:12:23 -0200

    Isolate packages

---
+++ app/routes/application.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import YeboApplicationRouteMixin from 'yebo-ember-auth/mixins/application-route-mixin';
+
+export default Ember.Route.extend(YeboApplicationRouteMixin);
--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,20 @@
 'use strict';
 
 module.exports = function(/* environment, appConfig */) {
-  return { };
+  return {
+    "yebo": {
+      signinPath: 'signin',
+      signupPath: 'signup',
+      accountPath: 'account'
+    },
+    "ember-simple-auth": {
+      localStorageKey: 'yebo-ember:session',
+      authorizer: 'ember-simple-auth-authorizer:yebo',
+      crossOriginWhitelist: ['http://localhost:3000'],
+      authenticationRoute: 'yebo.signin',
+      // routeAfterAuthenticationWithItems: "yebo.checkout",
+      // routeAfterAuthenticationWithoutItems: "yebo.account",
+      routeIfAlreadyAuthenticated: "yebo.account"
+    }
+  };
 };
